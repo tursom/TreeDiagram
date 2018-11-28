d5f031d9a2719287a9c65b1087be18e50a681256
tursom <tursom@foxmail.com>
2018-11-28 18:11:30 +0800

    优化项目结构

--- src/main/kotlin/cn/tursom/treediagram/RequestHandler.kt
+++ src/main/kotlin/cn/tursom/treediagram/RequestHandler.kt
@@ -9,13 +9,6 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 /**
- * 模组调用的返回值
- * @param state 是否成功调用模组
- * @param result 模组的返回值，结构由模组自定义，亦可以没有返回值
- */
-data class ReturnData(val state: Boolean, val result: Any?)
-
-/**
  * 用于处理一个模组调用请求
  * @param request Servlet的request对象
  */
